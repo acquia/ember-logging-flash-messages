7a353c977c2629e7232308825abea69916696100
Arvin Singla <arvin.singla@acquia.com>
2017-03-06 10:53:11 -0500

    Upgraded addon to Ember 2.11

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -5,8 +5,8 @@ import config from '../../config/environment';
 export default function startApp(attrs) {
   let application;
 
-  // use defaults, but you can override
-  let attributes = Ember.assign({}, config.APP, attrs); // jscs:ignore
+  let attributes = Ember.merge({}, config.APP); // jscs:ignore
+  attributes = Ember.merge(attributes, attrs); // jscs:ignore
 
   Ember.run(() => { // jscs:ignore
     application = Application.create(attributes);
